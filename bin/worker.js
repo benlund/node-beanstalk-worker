8df26bdce4ad9a62c430532c02aae73d3f63ca6b
Ben Lund <ben@benlund.co.uk>
2010-02-26 15:16:49 -0800

    bug fix in loading handlers

--- bin/worker.js
+++ bin/worker.js
@@ -1,7 +1,7 @@
 var BeanstalkWorkerCluster = require('../lib/beanstalk_worker_cluster').BeanstalkWorkerCluster;
 
 var handlers = {};
-process.mixin(handlers, require('../handlers/test'), require('../handlers/httprequest'));
+process.mixin(handlers, require('../handlers/test'), require('../handlers/http_request'));
 
 process.addListener('SIGINT', function() {
   BeanstalkWorkerCluster.stop();
